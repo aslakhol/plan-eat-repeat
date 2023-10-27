55017106cffa5e3ab2c64423909ab164931ade39
Madeleine Lorås <madeleine.loras@gmail.com>
2023-10-27 21:07:41 +0200

    added dinner component and title

--- src/views/Dinner.tsx
+++ src/views/Dinner.tsx
@@ -1,5 +1,51 @@
+import { Button } from "~/components/ui/button";
+
 type Props = {};
 
-export const Dinners = ({}: Props) => {
-  return <>Dinners</>;
+export const Dinner = ({}: Props) => {
+  return (
+    <>
+      <div className="flex flex-col rounded border px-4 py-2">
+        <h3 className="font-semibold">Dinner 1</h3>
+        <div className="mt-2 flex flex-wrap gap-2">
+          <Button
+            className="rounded bg-blue-100 px-2 py-1 text-blue-800 active:bg-blue-200"
+            variant="ghost"
+          >
+            Tag1
+          </Button>
+          <Button
+            className="rounded bg-red-100 px-2 py-1 text-red-800 active:bg-red-200"
+            variant="ghost"
+          >
+            Tag2
+          </Button>
+          <Button
+            className="rounded bg-green-100 px-2 py-1 text-green-800 active:bg-green-200"
+            variant="ghost"
+          >
+            Tag3
+          </Button>
+          <Button
+            className="rounded bg-yellow-100 px-2 py-1 text-yellow-800 active:bg-yellow-200"
+            variant="ghost"
+          >
+            Tag4
+          </Button>
+          <Button
+            className="rounded bg-purple-100 px-2 py-1 text-purple-800 active:bg-purple-200"
+            variant="ghost"
+          >
+            Tag5
+          </Button>
+          <Button
+            className="rounded bg-pink-100 px-2 py-1 text-pink-800 active:bg-pink-200"
+            variant="ghost"
+          >
+            Tag6
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,6 +1,11 @@
+import { api } from "~/utils/api";
 import { Button } from "../components/ui/button";
+import { Dinner } from "./Dinner";
 
 export const MainView = () => {
+  const tagQuery = api.dinner.tags.useQuery();
+  const dinnerQuery = api.dinner.dinners.useQuery();
+
   return (
     <div className="grid h-screen grid-cols-2">
       <div className="flex flex-col justify-between space-y-8 overflow-y-auto p-6">
@@ -9,47 +14,9 @@ export const MainView = () => {
             <h2 className="text-xl font-bold">Dinners</h2>
           </div>
           <div className="space-y-4">
-            <div className="flex flex-col rounded border px-4 py-2">
-              <h3 className="font-semibold">Dinner 1</h3>
-              <div className="mt-2 flex flex-wrap gap-2">
-                <Button
-                  className="rounded bg-blue-100 px-2 py-1 text-blue-800 active:bg-blue-200"
-                  variant="ghost"
-                >
-                  Tag1
-                </Button>
-                <Button
-                  className="rounded bg-red-100 px-2 py-1 text-red-800 active:bg-red-200"
-                  variant="ghost"
-                >
-                  Tag2
-                </Button>
-                <Button
-                  className="rounded bg-green-100 px-2 py-1 text-green-800 active:bg-green-200"
-                  variant="ghost"
-                >
-                  Tag3
-                </Button>
-                <Button
-                  className="rounded bg-yellow-100 px-2 py-1 text-yellow-800 active:bg-yellow-200"
-                  variant="ghost"
-                >
-                  Tag4
-                </Button>
-                <Button
-                  className="rounded bg-purple-100 px-2 py-1 text-purple-800 active:bg-purple-200"
-                  variant="ghost"
-                >
-                  Tag5
-                </Button>
-                <Button
-                  className="rounded bg-pink-100 px-2 py-1 text-pink-800 active:bg-pink-200"
-                  variant="ghost"
-                >
-                  Tag6
-                </Button>
-              </div>
-            </div>
+            {dinnerQuery.data?.dinners.map((dinner) => {
+              return <Dinner key={dinner.id} />;
+            })}
             <div className="flex flex-col rounded border px-4 py-2">
               <h3 className="font-semibold">Dinner 2</h3>
               <div className="mt-2 flex flex-wrap gap-2">
