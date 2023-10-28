15a6dc6e8b4f15d3fe1b2502492d3300d75fc0de
Aslak Hollund <aslakhol@gmail.com>
2023-10-28 14:51:48 +0200

    make days by mapping array of days

--- src/views/WeekPlan.tsx
+++ src/views/WeekPlan.tsx
@@ -1,19 +1,37 @@
-type Props = {};
+import { type Dinner } from "@prisma/client";
+
+type Props = { selectedDinnerIds: number[] };
 
 export const WeekPlan = ({}: Props) => {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
   return (
     <div className="flex flex-col items-end justify-start bg-gray-50 p-6 dark:bg-gray-900">
       <h2 className="mb-8 text-right text-xl font-bold">Week Plan</h2>
       <div className="w-full space-y-4 text-right">
-        <div className="flex flex-col rounded border px-4 py-2">
-          <h3 className="font-semibold">Monday</h3>
-          <p className="mt-2">No dinner selected</p>
-        </div>
-        <div className="flex flex-col rounded border px-4 py-2">
-          <h3 className="font-semibold">Tuesday</h3>
-          <p className="mt-2">No dinner selected</p>
-        </div>
+        {days.map((day) => (
+          <Day key={day} day={day} dinner={undefined} />
+        ))}
       </div>
+    </div>
+  );
+};
+
+type DayProps = { day: string; dinner?: Dinner };
+
+const Day = ({ day, dinner }: DayProps) => {
+  return (
+    <div className="flex flex-col rounded border px-4 py-2">
+      <h3 className="font-semibold">{day}</h3>
+      <p className="mt-2">{dinner ? dinner.name : "No dinner selected"}</p>
     </div>
   );
 };
