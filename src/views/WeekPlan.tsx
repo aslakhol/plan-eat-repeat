273a7a669d0c4be107500b9b6b6971bdba85723b
Aslak Hollund <aslakhol@gmail.com>
2023-11-12 15:21:54 +0100

    separate render for selected dinner and not

--- src/views/WeekPlan.tsx
+++ src/views/WeekPlan.tsx
@@ -33,27 +33,21 @@ const Day = ({ day, dinner }: DayProps) => {
   return (
     <div className="flex flex-col rounded border px-2 py-2">
       <h3 className="mb-2 mr-1 text-xs">{day}</h3>
-      <SelectedDinner dinner={dinner} />
+      <Dinner dinner={dinner} />
     </div>
   );
 };
 
-type SelectedDinnerProps = { dinner?: Dinner };
+type DinnerProps = { dinner?: Dinner };
 
-const SelectedDinner = ({ dinner }: SelectedDinnerProps) => {
+const Dinner = ({ dinner }: DinnerProps) => {
   if (!dinner) {
-    return (
-      <div className="rounded-md border p-1">
-        <p className={cn("mt-2 min-h-[24px]", dinner && "font-semibold")} />
-      </div>
-    );
+    return <div className="h-12 rounded-md border"></div>;
   }
 
   return (
-    <div className="rounded-md border p-1">
-      <p className={cn("mt-2 min-h-[24px]", dinner && "font-semibold")}>
-        {dinner ? dinner.name : ""}
-      </p>
+    <div className="flex h-12 flex-col-reverse rounded-md border p-1 hover:bg-slate-100">
+      <p className={cn("font-semibold")}>{dinner.name}</p>
     </div>
   );
 };
