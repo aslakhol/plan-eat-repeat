481d353917444483b90ec1dc748b3d632f78c973
Aslak Hollund <aslakhol@gmail.com>
2024-10-21 20:57:05 +0200

    add new tag form

--- src/views/NewDinners/DinnerForm.tsx
+++ src/views/NewDinners/DinnerForm.tsx
@@ -26,6 +26,7 @@ export const DinnerForm = ({ existingDinner }: Props) => {
     defaultValues: {
       name: existingDinner?.name,
       tags: existingDinner?.tags.map((tag) => tag.value),
+      newTag: "",
       link: "",
       notes: "",
     },
@@ -34,6 +35,22 @@ export const DinnerForm = ({ existingDinner }: Props) => {
   function onSubmit(values: z.infer<typeof dinnerFormSchema>) {
     console.log(values);
   }
+  const tags = form.watch("tags");
+  const newTag = form.watch("newTag");
+
+  const addTag = () => {
+    if (!newTag) return;
+    if (tags.includes(newTag)) return;
+    form.setValue("tags", [...tags, newTag]);
+    form.setValue("newTag", "");
+  };
+
+  const removeTag = (tag: string) => {
+    form.setValue(
+      "tags",
+      tags.filter((t) => t !== tag),
+    );
+  };
 
   return (
     <Form {...form}>
@@ -58,18 +75,33 @@ export const DinnerForm = ({ existingDinner }: Props) => {
               <div
                 className="rounded bg-green-100 px-2 py-1 text-green-800 active:bg-green-200"
                 key={tag}
-                onClick={() =>
-                  form.setValue(
-                    "tags",
-                    form.watch("tags").filter((t) => t !== tag),
-                  )
-                }
+                onClick={() => removeTag(tag)}
               >
                 {tag}
               </div>
             ))}
           </div>
         </div>
+
+        <FormField
+          control={form.control}
+          name="newTag"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Add tag</FormLabel>
+              <FormControl>
+                <div className="flex w-full max-w-sm items-center space-x-2">
+                  <Input {...field} />
+                  <Button type="button" variant="outline" onClick={addTag}>
+                    +
+                  </Button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <FormField
           control={form.control}
           name="link"
@@ -105,6 +137,7 @@ export const DinnerForm = ({ existingDinner }: Props) => {
 const dinnerFormSchema = z.object({
   name: z.string().min(1),
   tags: z.array(z.string()),
+  newTag: asOptionalStringWithoutEmpty(z.string().max(20).min(1)),
   link: asOptionalStringWithoutEmpty(z.string().url()),
   notes: asOptionalStringWithoutEmpty(z.string()),
 });
