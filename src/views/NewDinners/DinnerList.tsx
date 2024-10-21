1b5a96fb0c04df79841ed8e66db9e14b1951f569
Aslak Hollund <aslakhol@gmail.com>
2024-10-21 21:32:32 +0200

    pass close to form

--- src/views/NewDinners/DinnerList.tsx
+++ src/views/NewDinners/DinnerList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -15,36 +16,51 @@ export const DinnerList = ({ dinners }: Props) => {
   return (
     <div className="flex flex-col gap-4">
       {dinners.map((d) => (
-        <Dialog key={d.id}>
-          <DialogTrigger>
-            <div
-              className={cn(
-                "flex cursor-pointer flex-col rounded-md border px-4 py-2 text-left hover:bg-accent/50 hover:text-accent-foreground",
-              )}
-            >
-              <h3 className="font-semibold">{d.name}</h3>
-              <div className="mt-2 flex flex-wrap gap-2">
-                {d.tags.map((tag) => {
-                  return (
-                    <div
-                      key={tag.value}
-                      className="rounded bg-green-100 px-2 py-1 text-green-800 active:bg-green-200"
-                    >
-                      {tag.value}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>{d.name}</DialogTitle>
-            </DialogHeader>
-            <DinnerForm existingDinner={d} />
-          </DialogContent>
-        </Dialog>
+        <DinnerListItem key={d.id} dinner={d} />
       ))}
     </div>
+  );
+};
+
+type DinnerListItemProps = {
+  dinner: DinnerWithTags;
+};
+
+const DinnerListItem = ({ dinner }: DinnerListItemProps) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+      <DialogTrigger>
+        <div
+          className={cn(
+            "flex cursor-pointer flex-col rounded-md border px-4 py-2 text-left hover:bg-accent/50 hover:text-accent-foreground",
+          )}
+        >
+          <h3 className="font-semibold">{dinner.name}</h3>
+          <div className="mt-2 flex flex-wrap gap-2">
+            {dinner.tags.map((tag) => {
+              return (
+                <div
+                  key={tag.value}
+                  className="rounded bg-green-100 px-2 py-1 text-green-800 active:bg-green-200"
+                >
+                  {tag.value}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{dinner.name}</DialogTitle>
+        </DialogHeader>
+        <DinnerForm
+          existingDinner={dinner}
+          closeDialog={() => setDialogOpen(false)}
+        />
+      </DialogContent>
+    </Dialog>
   );
 };
