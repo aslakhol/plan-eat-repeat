1e45c99c448c7391f8e5897bb69b2fbe3db98592
Aslak Hollund <aslakhol@gmail.com>
2024-10-25 17:14:06 +0200

    temp change to planned dinner

--- src/views/NewPlan/PlannedDinner.tsx
+++ src/views/NewPlan/PlannedDinner.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../components/ui/button";
 import { api } from "../../utils/api";
 import { type DinnerWithTags } from "../../utils/types";
 import { format, isSameDay, startOfWeek } from "date-fns";
+import { DialogHeader, DialogTitle } from "../../components/ui/dialog";
 
 type Props = {
   dinner: DinnerWithTags;
@@ -12,43 +13,49 @@ type Props = {
 
 export const PlannedDinner = ({ dinner, date, closeDialog }: Props) => {
   return (
-    <div className="flex flex-col gap-2">
-      <h1 className="text-lg font-semibold">{dinner.name}</h1>
-      {dinner.tags.length > 0 && (
-        <div className="flex flex-wrap gap-2">
-          {dinner.tags.map((tag) => (
-            <div
-              className="rounded border border-green-100 bg-green-100 px-2 py-1 text-green-800"
-              key={tag.value}
-            >
-              {tag.value}
-            </div>
-          ))}
-        </div>
-      )}
-      {dinner.link && (
-        <a
-          className="line-clamp-1 max-w-md text-sm text-blue-500 underline"
-          href={dinner.link}
-          target="_blank"
-        >
-          {dinner.link}
-        </a>
-      )}
-      {dinner.notes && (
-        <div className="min-h-[100px]">
-          {dinner.notes.split("\n").map((line) => (
-            <p key={line}>{line}</p>
-          ))}
-        </div>
-      )}
+    <>
+      <DialogHeader>
+        <DialogTitle>{format(date, "EEE do")}</DialogTitle>
+        {/* <DialogDescription>{dinner.name}</DialogDescription> */}
+      </DialogHeader>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-lg font-semibold">{dinner.name}</h1>
+        {dinner.tags.length > 0 && (
+          <div className="flex flex-wrap gap-2">
+            {dinner.tags.map((tag) => (
+              <div
+                className="rounded border border-green-100 bg-green-100 px-2 py-1 text-green-800"
+                key={tag.value}
+              >
+                {tag.value}
+              </div>
+            ))}
+          </div>
+        )}
+        {dinner.link && (
+          <a
+            className="line-clamp-1 max-w-md text-sm text-blue-500 underline"
+            href={dinner.link}
+            target="_blank"
+          >
+            {dinner.link}
+          </a>
+        )}
+        {dinner.notes && (
+          <div className="min-h-[100px]">
+            {dinner.notes.split("\n").map((line) => (
+              <p key={line}>{line}</p>
+            ))}
+          </div>
+        )}
 
-      <div className="flex w-full gap-2">
-        <Button variant={"outline"}>Change plan</Button>
-        <ClearDay date={date} closeDialog={closeDialog} />
-        <Button variant={"outline"}>Edit dinner</Button>
+        <div className="flex w-full gap-2">
+          <Button variant={"outline"}>Change plan</Button>
+          <ClearDay date={date} closeDialog={closeDialog} />
+          <Button variant={"outline"}>Edit dinner</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
