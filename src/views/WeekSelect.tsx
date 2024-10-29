d51edf8656493697bbedbf02ca9ec0e542387350
Aslak Hollund <aslakhol@gmail.com>
2024-10-29 18:18:10 +0100

    get rid of a fragment and a div

--- src/views/WeekSelect.tsx
+++ src/views/WeekSelect.tsx
@@ -13,38 +13,34 @@ export const WeekSelect = ({
   startOfDisplayedWeek,
 }: WeekSelectProps) => {
   return (
-    <>
-      <div>
-        <div className="flex items-center gap-2">
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => setWeekOfSet((prev) => prev - 1)}
-          >
-            <span className="sr-only">Go back 1 week</span>
-            <ChevronLeft className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => setWeekOfSet(0)}
-          >
-            <span className="sr-only">Go to today</span>
-            <Calendar className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="outline"
-            className="h-8 w-8 p-0"
-            onClick={() => setWeekOfSet((prev) => prev + 1)}
-          >
-            <span className="sr-only">Go forward 1 week</span>
-            <ChevronRight className="h-4 w-4" />
-          </Button>
-          <div className="px-4 text-sm font-medium">
-            Week {format(startOfDisplayedWeek, "w, MMMM, yyyy")}
-          </div>
-        </div>
+    <div className="flex items-center gap-2">
+      <Button
+        variant="outline"
+        className="h-8 w-8 p-0"
+        onClick={() => setWeekOfSet((prev) => prev - 1)}
+      >
+        <span className="sr-only">Go back 1 week</span>
+        <ChevronLeft className="h-4 w-4" />
+      </Button>
+      <Button
+        variant="outline"
+        className="h-8 w-8 p-0"
+        onClick={() => setWeekOfSet(0)}
+      >
+        <span className="sr-only">Go to today</span>
+        <Calendar className="h-4 w-4" />
+      </Button>
+      <Button
+        variant="outline"
+        className="h-8 w-8 p-0"
+        onClick={() => setWeekOfSet((prev) => prev + 1)}
+      >
+        <span className="sr-only">Go forward 1 week</span>
+        <ChevronRight className="h-4 w-4" />
+      </Button>
+      <div className="px-4 text-sm font-medium">
+        Week {format(startOfDisplayedWeek, "w, MMMM, yyyy")}
       </div>
-    </>
+    </div>
   );
 };
