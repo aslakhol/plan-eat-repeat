85b8d5c89222b577383f080aa9949c9fe3b957ee
Aslak Hollund <aslakhol@gmail.com>
2024-11-01 22:43:55 +0100

    update filters

--- src/views/Filter.tsx
+++ src/views/Filter.tsx
@@ -2,8 +2,9 @@ import { type Dispatch, type SetStateAction } from "react";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { cn } from "../lib/utils";
-import { FilterIcon } from "lucide-react";
+import { FilterIcon, X } from "lucide-react";
 import { api } from "../utils/api";
+import { Badge } from "../components/ui/badge";
 
 type Props = {
   search: string;
@@ -62,24 +63,33 @@ const Tags = ({ selectedTags, setSelectedTags }: TagsProps) => {
   return (
     <div className="flex flex-wrap gap-2 pt-2">
       {filtersQuery.data?.tags.map((tag) => (
-        <div
+        <Badge
           key={tag.value}
+          variant="secondary"
           className={cn(
-            "cursor-pointer rounded border border-green-100 bg-green-100 px-2 py-1 text-green-800 hover:bg-green-300",
+            "cursor-pointer hover:bg-secondary-foreground/10",
             selectedTags.includes(tag.value) &&
-              "border border-green-400 bg-green-200 ",
+              "border border-secondary-foreground/50",
           )}
           onClick={() => {
             if (selectedTags.includes(tag.value)) {
               setSelectedTags((prev) => prev.filter((t) => t !== tag.value));
-              return;
+            } else {
+              setSelectedTags((prev) => [...prev, tag.value]);
             }
-            setSelectedTags((prev) => [...prev, tag.value]);
           }}
         >
           {tag.value}
-        </div>
+        </Badge>
       ))}
+      <Badge
+        variant="secondary"
+        className={cn("cursor-pointer hover:bg-secondary-foreground/10")}
+        onClick={() => setSelectedTags([])}
+      >
+        Clear filters
+        <X className="ml-1 h-3 w-3 text-muted-foreground hover:text-foreground" />
+      </Badge>
     </div>
   );
 };
