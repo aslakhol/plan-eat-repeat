78826f6d9d0310565147a45b16ab9cdd8f9dde73
Aslak Hollund <aslakhol@gmail.com>
2024-11-01 22:37:50 +0100

    move tags into filter component

--- src/views/Filter.tsx
+++ src/views/Filter.tsx
@@ -2,8 +2,8 @@ import { type Dispatch, type SetStateAction } from "react";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { cn } from "../lib/utils";
-import { Tags } from "./Dinners/Tags";
 import { FilterIcon } from "lucide-react";
+import { api } from "../utils/api";
 
 type Props = {
   search: string;
@@ -47,6 +47,39 @@ export const Filter = ({
       {showTags && (
         <Tags selectedTags={selectedTags} setSelectedTags={setSelectedTags} />
       )}
+    </div>
+  );
+};
+
+type TagsProps = {
+  selectedTags: string[];
+  setSelectedTags: Dispatch<SetStateAction<string[]>>;
+};
+
+const Tags = ({ selectedTags, setSelectedTags }: TagsProps) => {
+  const filtersQuery = api.dinner.tags.useQuery();
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      {filtersQuery.data?.tags.map((tag) => (
+        <div
+          key={tag.value}
+          className={cn(
+            "cursor-pointer rounded border border-green-100 bg-green-100 px-2 py-1 text-green-800 hover:bg-green-300",
+            selectedTags.includes(tag.value) &&
+              "border border-green-400 bg-green-200 ",
+          )}
+          onClick={() => {
+            if (selectedTags.includes(tag.value)) {
+              setSelectedTags((prev) => prev.filter((t) => t !== tag.value));
+              return;
+            }
+            setSelectedTags((prev) => [...prev, tag.value]);
+          }}
+        >
+          {tag.value}
+        </div>
+      ))}
     </div>
   );
 };
