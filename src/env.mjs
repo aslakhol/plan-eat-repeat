2782f7b8c69377f9448f5fdf11782d24b3d5bf44
Aslak Hollund <aslakhol@gmail.com>
2024-11-03 21:27:58 +0100

    add clerk envs to env check

--- src/env.mjs
+++ src/env.mjs
@@ -26,6 +26,7 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_POSTHOG_API_KEY: z.string().min(1),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
 
   /**
@@ -36,6 +37,8 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_POSTHOG_API_KEY: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
