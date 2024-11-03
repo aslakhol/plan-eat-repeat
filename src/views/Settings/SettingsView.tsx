f0a6f1525d07912903cc57b89291a1000e8409db
Aslak Hollund <aslakhol@gmail.com>
2024-11-03 17:05:33 +0100

    create org if none exists

--- src/views/Settings/SettingsView.tsx
+++ src/views/Settings/SettingsView.tsx
@@ -5,7 +5,12 @@ import { cn } from "../../lib/utils";
 import Link from "next/link";
 
 export const SettingsView = () => {
-  const { openUserProfile, openOrganizationProfile } = useClerk();
+  const {
+    openUserProfile,
+    openOrganizationProfile,
+    openCreateOrganization,
+    organization,
+  } = useClerk();
 
   return (
     <div className="flex flex-col gap-4 p-4">
@@ -16,13 +21,24 @@ export const SettingsView = () => {
       >
         Account
       </Button>
-      <Button
-        className={cn("justify-start")}
-        variant={"outline"}
-        onClick={() => openOrganizationProfile()}
-      >
-        Organization
-      </Button>
+      {organization ? (
+        <Button
+          className={cn("justify-start")}
+          variant={"outline"}
+          onClick={() => openOrganizationProfile()}
+        >
+          Organization
+        </Button>
+      ) : (
+        <Button
+          className={cn("justify-start")}
+          variant={"outline"}
+          onClick={() => openCreateOrganization()}
+        >
+          Create Organization
+        </Button>
+      )}
+
       <SignOutButton>
         <Button variant={"outline"} className={cn("justify-start")}>
           Sign out
