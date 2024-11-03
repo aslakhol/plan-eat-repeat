8f2eb64f2b2802443899ce86cd0c55474ecc9c29
Aslak Hollund <aslakhol@gmail.com>
2024-11-03 17:26:34 +0100

    sign in button on settings if signed out

--- src/views/Settings/SettingsView.tsx
+++ src/views/Settings/SettingsView.tsx
@@ -1,4 +1,10 @@
-import { SignOutButton, useClerk } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  useClerk,
+} from "@clerk/nextjs";
 import { BottomNav } from "../BottomNav";
 import { Button } from "../../components/ui/button";
 import { cn } from "../../lib/utils";
@@ -12,42 +18,51 @@ export const SettingsView = () => {
     organization,
   } = useClerk();
 
-  console.log(organization);
-
   return (
     <>
-      <div className="flex flex-col gap-4 p-4">
-        <Button
-          className={cn("justify-start")}
-          variant={"outline"}
-          onClick={() => openUserProfile()}
-        >
-          Account
-        </Button>
-        {organization ? (
+      <SignedIn>
+        <div className="flex flex-col gap-4 p-4">
           <Button
             className={cn("justify-start")}
             variant={"outline"}
-            onClick={() => openOrganizationProfile()}
+            onClick={() => openUserProfile()}
           >
-            Organization
+            Account
           </Button>
-        ) : (
-          <Button
-            className={cn("justify-start")}
-            variant={"outline"}
-            onClick={() => openCreateOrganization()}
-          >
-            Create Organization
-          </Button>
-        )}
+          {organization ? (
+            <Button
+              className={cn("justify-start")}
+              variant={"outline"}
+              onClick={() => openOrganizationProfile()}
+            >
+              Organization
+            </Button>
+          ) : (
+            <Button
+              className={cn("justify-start")}
+              variant={"outline"}
+              onClick={() => openCreateOrganization()}
+            >
+              Create Organization
+            </Button>
+          )}
 
-        <SignOutButton>
-          <Button variant={"outline"} className={cn("justify-start")}>
-            Sign out
-          </Button>
-        </SignOutButton>
-      </div>
+          <SignOutButton>
+            <Button variant={"outline"} className={cn("justify-start")}>
+              Sign out
+            </Button>
+          </SignOutButton>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <div className="flex flex-col gap-4 p-4">
+          <SignInButton>
+            <Button variant={"outline"} className={cn("justify-start")}>
+              Sign in
+            </Button>
+          </SignInButton>
+        </div>
+      </SignedOut>
       <BottomNav />
     </>
   );
