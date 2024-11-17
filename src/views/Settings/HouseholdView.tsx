d364283d9c053a3fb62fcf72e50c5f188a3b4c78
Aslak Hollund <aslakhol@gmail.com>
2024-11-17 13:32:13 +0100

    render invites if you are admin

--- src/views/Settings/HouseholdView.tsx
+++ src/views/Settings/HouseholdView.tsx
@@ -3,10 +3,21 @@ import { BottomNav } from "../BottomNav";
 import { NewHousehold } from "./HouseholdForm";
 import { EditHousehold } from "./HouseholdForm";
 import { Memberships } from "./Memberships";
+import { Invites } from "./Invites";
+import { api } from "../../utils/api";
+import { useClerk } from "@clerk/nextjs";
 
 type Props = { currentHousehold?: Household };
 
 export const HouseholdView = ({ currentHousehold }: Props) => {
+  const { user } = useClerk();
+  const membersQuery = api.household.members.useQuery(
+    { householdId: currentHousehold?.id ?? "" },
+    { enabled: !!currentHousehold },
+  );
+  const userIsAdmin = !!membersQuery.data?.members.some(
+    (member) => member.userId === user?.id && member.role === "ADMIN",
+  );
   return (
     <div>
       {!currentHousehold ? (
@@ -15,6 +26,7 @@ export const HouseholdView = ({ currentHousehold }: Props) => {
         <div className=" space-y-8 p-4">
           <EditHousehold household={currentHousehold} />
           <Memberships household={currentHousehold} />
+          {userIsAdmin && <Invites household={currentHousehold} />}
         </div>
       )}
       <BottomNav />
