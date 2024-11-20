fa8352decca05a5cf0e72223686988a12dd97b78
Aslak Hollund <aslakhol@gmail.com>
2024-11-20 20:14:29 +0100

    do upserts in webhooks to at least be a bit better

--- src/server/webhooks/user.ts
+++ src/server/webhooks/user.ts
@@ -1,33 +1,35 @@
 import { type UserJSON } from "@clerk/nextjs/dist/types/server";
 import { db } from "../db";
 
-export const userCreated = async (data: UserJSON) => {
-  const user = await db.user.create({
-    data: {
-      id: data.id,
-      firstName: data.first_name,
-      lastName: data.last_name,
-    },
-  });
-
-  return user;
-};
-
 export const userDeleted = async (id?: string) => {
   if (!id) {
     throw new Error("No id provided");
   }
 
-  await db.user.delete({
+  return db.user.delete({
     where: {
       id,
     },
   });
 };
 
+export const userCreated = async (data: UserJSON) => {
+  return upsertUser(data);
+};
+
 export const userUpdated = async (data: UserJSON) => {
-  await db.user.update({
+  return upsertUser(data);
+};
+
+const upsertUser = async (data: UserJSON) => {
+  const user = await db.user.upsert({
     where: { id: data.id },
-    data: { firstName: data.first_name, lastName: data.last_name },
+    update: { firstName: data.first_name, lastName: data.last_name },
+    create: {
+      id: data.id,
+      firstName: data.first_name,
+      lastName: data.last_name,
+    },
   });
+  return user;
 };
