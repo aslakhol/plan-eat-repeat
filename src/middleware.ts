a084f81e4ab2584d8de9c89231c836b40397bfe4
Aslak Hollund <aslakhol@gmail.com>
2024-11-21 21:39:55 +0100

    hopefully valid link in matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/invite/*"]);
+const isPublicRoute = createRouteMatcher(["/", "/invite/:inviteId"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId, sessionClaims, redirectToSignIn } = await auth();
