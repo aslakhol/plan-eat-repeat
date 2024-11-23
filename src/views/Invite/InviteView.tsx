e96c4b76c6db6ca0f4c683e630645ca200d28258
Aslak Hollund <aslakhol@gmail.com>
2024-11-23 12:19:30 +0100

    don't throw errors when visiting invitation and logged out

--- src/views/Invite/InviteView.tsx
+++ src/views/Invite/InviteView.tsx
@@ -4,10 +4,40 @@ import { api } from "../../utils/api";
 import { NoInvite } from "./NoInvite";
 import { Invitation } from "./Invitation";
 import { AlreadyInHousehold } from "./AlreadyInHousehold";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 type Props = { inviteId: string };
 
 export const InviteView = ({ inviteId }: Props) => {
+  const inviteQuery = api.household.getInvite.useQuery({ inviteId });
+
+  if (inviteQuery.isLoading) {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <UtensilsCrossed className="animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <SignedIn>
+        <LoggedInInvite inviteId={inviteId} />
+      </SignedIn>
+      <SignedOut>
+        {!inviteQuery.data?.invite ? (
+          <NoInvite />
+        ) : (
+          <Invitation invite={inviteQuery.data.invite} />
+        )}
+      </SignedOut>
+    </>
+  );
+};
+
+type LoggedInInviteProps = { inviteId: string };
+
+export const LoggedInInvite = ({ inviteId }: LoggedInInviteProps) => {
   const inviteQuery = api.household.getInvite.useQuery({ inviteId });
   const householdQuery = api.household.household.useQuery();
 
