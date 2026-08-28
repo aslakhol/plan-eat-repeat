01dcffe0c1e33ab5195e7437bff263eedf86e8e0
Aslak Hollund <aslakhol@gmail.com>
2026-08-28 15:54:03 +0200

    Trigger chicken dinner for kylling

--- apps/web/src/lib/chicken-dinner.ts
+++ apps/web/src/lib/chicken-dinner.ts
@@ -1,8 +1,10 @@
-const CHICKEN_WORD = /(?<![\p{L}\p{N}_])chicken(?![\p{L}\p{N}_])/iu;
+const CHICKEN_WORD =
+  /(?<![\p{L}\p{N}_])(?:chicken|kylling)(?![\p{L}\p{N}_])/iu;
 
-export const isChickenDinnerTitle = (title: string) => CHICKEN_WORD.test(title);
+export const isChickenDinner = (title: string, tags: readonly string[] = []) =>
+  CHICKEN_WORD.test(title) || tags.some((tag) => CHICKEN_WORD.test(tag));
 
-export const saveDinnerLabel = (title: string) =>
-  isChickenDinnerTitle(title)
+export const saveDinnerLabel = (title: string, tags: readonly string[] = []) =>
+  isChickenDinner(title, tags)
     ? "Winner, winner, chicken dinner."
     : "Save dinner";
